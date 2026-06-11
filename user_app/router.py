29c56cd141f7eb6d6e65a094266a82ae76304d91
FeliksNovoselskyi <felixdnbox@gmail.com>
2026-06-11 18:40:21 +0300

    Add pagination examples

--- user_app/router.py
+++ user_app/router.py
@@ -1,14 +1,14 @@
 import rest_framework.routers as routers
 from .api_views import *
 from django.urls import path
-from user_app.viewsets.book import BookViewSet
+from user_app.viewsets.book import BookViewset
 from user_app.viewsets.car import CarViewSet
 
 router = routers.DefaultRouter()
 
 
 router.register(r"cars", CarViewSet)
-router.register(r"books", BookViewSet)
+router.register(r"books", BookViewset)
 
 # urlpatterns = [
 #     path("cars/all/", AllCarsApiView.as_view()),
--- user_app/viewsets/book.py
+++ user_app/viewsets/book.py
@@ -1,41 +1,67 @@
 import rest_framework.viewsets as viewsets
-from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from rest_framework_simplejwt.authentication import  JWTAuthentication
 from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination, CursorPagination
 
 from django_filters.rest_framework import DjangoFilterBackend
 
 from user_app.models import Book
 from user_app.serializers.book import BookSerializer
 
-class BookViewSet(viewsets.ModelViewSet):
+
+# class BookPaginator(PageNumberPagination):
     
+#     page_size = 5
+#     page_query_param = "page"
+#     page_size_query_param = "size"
+#     max_page_size = 90
+#     last_page_strings = ["last"]
+
+
+# class BookPaginator(LimitOffsetPagination):
+    
+#     default_limit = 5
+#     max_limit = 30
+
+class BookPaginator(CursorPagination):
+
+    page_size = 2
+    
+    ordering = "id"
+    
+    page_size_query_param = "size"
+
+
+
+class BookViewset(viewsets.ModelViewSet):
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
+    pagination_class = BookPaginator 
+
     authentication_classes = [
         JWTAuthentication
     ]
-    
+
     filter_backends = [
         DjangoFilterBackend,
         filters.SearchFilter,
         filters.OrderingFilter
     ]
 
-    # Filtering
-    filterset_fields = [
-        "title",
-        "author"
+    filtering_fields = [
+        "title"
     ]
-    
-    # Searching
+
     search_fields = [
         "title",
         "author__username"
     ]
-    
-    # Ordering
+
     ordering_fields = [
         "published_date"
     ]
-    ordering = ["title"] # Поля, которое стандартно будет использоваться для фильтрации
+
+    ordering = ["id"]
+    
